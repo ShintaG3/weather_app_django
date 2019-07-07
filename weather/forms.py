a88e87969f9c8ed057d0bd418d168db5409f58d7
ShintaG3 <snishino2013@gmail.com>
2019-07-07 19:55:16 +0900

    firstcommit

---
+++ weather/forms.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+from django.forms import ModelForm, TextInput
+from .models import City 
+
+class CityForm(ModelForm):
+    class Meta:
+        model = City 
+        fields = ['name']
+=======
+from django.forms import ModelForm, TextInput
+from .models import City 
+
+class CityForm(ModelForm):
+    class Meta:
+        model = City 
+        fields = ['name']
+>>>>>>> first commit
+        widgets = {'name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'})}
